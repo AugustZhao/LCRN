1d868d9a7b899ce564bd3e9a8c0c61eb519b873c
zhaoxuebing <zhaoxuebing@xigua.city>
2021-12-15 13:45:26 +0800

    custorm App  adding images fonts and onBoarding

---
+++ src/screens/onBoarding/onBoarding.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+import {styles} from './onBoarding.style';
+
+export const OnBoarding = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>$functionComponent$</Text>
+    </SafeAreaView>
+  );
+};
